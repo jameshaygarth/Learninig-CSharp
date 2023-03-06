07ae694a7b211915be408e966f4cbbd881e801c7
Haygarth <jamesh@savant.co.uk>
2023-03-06 12:59:51 +0000

    adding section 9 exercise

---
+++ CSharp_basics_for_beginners/Section9_working_with_files/exercises/exercises/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+// See https://aka.ms/new-console-template for more information
+
+static int GetNumberOfWordsInStrin(string str)
+{
+    return str.Split(" ").Length;
+}
+static string GetLongestWordInString(string str)
+{
+    var words = str.Split(" ");
+    string longestWord = "";
+    foreach (var word in words)
+    {
+        if (longestWord.Length < word.Length)
+        {
+            longestWord = word;
+        }
+    }
+    return longestWord;
+}
+
+
+string txtFilePath = "C:\\temp\\tutorialRead.txt";
+Console.WriteLine("Create a text file \"{0}\"", txtFilePath);
+if (!File.Exists(txtFilePath))
+{
+    Console.WriteLine("File Doesnt Exist!");
+    return;
+}
+
+var fileContents = File.ReadAllText(txtFilePath);
+
+string longestWord = GetLongestWordInString(fileContents);
+int numberOfWords = GetNumberOfWordsInStrin(fileContents);
+Console.WriteLine("The longest word in the file is \"{0}\"", longestWord);
+Console.WriteLine("Number Of Words {0}", numberOfWords);
+
+
